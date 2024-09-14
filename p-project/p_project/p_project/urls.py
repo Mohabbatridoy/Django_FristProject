a2b8e68ebde7c0c8f7fbfde99be7e26ddc13d829
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-09-14 20:55:03 +0600

    pratice templates

--- p-project/p_project/p_project/urls.py
+++ p-project/p_project/p_project/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app_1/',include('app_1.urls'))
+    path('app_1/',include('app_1.urls')),
 ]
